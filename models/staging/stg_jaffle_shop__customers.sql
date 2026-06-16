d98c94ca5a5da27cc46627ecca9a0fc55e61d843
Yasmin Osman <34187761+yasmin-osman@users.noreply.github.com>
2026-06-16 21:10:03 +0100

    added sources: customers, orders and payments

--- models/staging/stg_jaffle_shop__customers.sql
+++ models/staging/stg_jaffle_shop__customers.sql
@@ -3,6 +3,6 @@ select
     first_name,
     last_name
 
-from raw.jaffle_shop.customers
+from {{ source('jaffle_shop', 'customers') }}
 
 
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -6,4 +6,4 @@ select
     amount / 100 as amount,
     created as created_at
 
-from raw.stripe.payments
+from {{ source('stripe', 'payments') }}
